f117cc978c1ba9d39c85a910d6f90f4fd9c0f7ff
bzancio <bzancio@gmail.com>
2026-05-19 09:19:18 +0200

    initial commit

---
+++ src/main/java/metrica/meka5/Meka5Application.java
@@ -0,0 +1,21 @@
+package metrica.meka5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Meka5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meka5Application.class, args);
+	}
+
+	@GetMapping("/Hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/metrica/meka5/Meka5ApplicationTests.java
@@ -0,0 +1,13 @@
+package metrica.meka5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meka5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
