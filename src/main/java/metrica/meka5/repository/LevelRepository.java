aacad117c2dabc8f95612204065b001cb8710e57
istoo11 <155294886+istoo11@users.noreply.github.com>
2026-05-25 14:47:29 +0200

    Revert "Develop database"

--- src/main/java/metrica/meka5/repository/LevelRepository.java
+++
@@ -1,9 +0,0 @@
-package metrica.meka5.repository;
-
-import metrica.meka5.models.Level;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LevelRepository extends JpaRepository<Level, Long> {
-}
