650596df26c564245ba6cfa7e6b2eb77b275b08f
bzancio <bzancio@gmail.com>
2026-06-03 11:34:51 +0200

    fixed caps

--- src/main/java/metrica/meka5/service/TypingService.java
+++ src/main/java/metrica/meka5/service/TypingService.java
@@ -1,5 +1,6 @@
 package metrica.meka5.service;
 
+import java.text.Normalizer;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -22,28 +23,33 @@ public class TypingService {
 	private CommonWordRepository commonWordRepository;
 
     private static final Pattern PUNCTUATION_PATTERN = Pattern.compile("\\p{Punct}");
+    private static final Pattern DIACRITIC_PATTERN = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
 
-	public List<String> getRandomWordsPackage(int packageSize, boolean includePunctuation, boolean maintainCase) {
+	public List<String> getRandomWordsPackage(int packageSize, boolean includePunctuation, boolean maintainCase, boolean removeDiacritics) {
 		List<CommonWord> commonWords = commonWordRepository.findRandomWords(packageSize);
 		return commonWords.stream()
 				.map(CommonWord::getWord)
-				.map(word -> transformWord(word, includePunctuation, maintainCase))
+				.map(word -> transformWord(word, includePunctuation, maintainCase, removeDiacritics))
 				.collect(Collectors.toList());
 	}
 	
-	public String getSentence(boolean includePunctuation, boolean maintainCase) {
+	public String getSentence(boolean includePunctuation, boolean maintainCase, boolean removeDiacritics) {
 		String quote = Objects.requireNonNull(restClient.get()
                         .retrieve()
                         .body(SentenceKanyeResponse.class))
 				.getQuote();
-		return transformWord(quote, includePunctuation, maintainCase);
+		return transformWord(quote, includePunctuation, maintainCase, removeDiacritics);
 	}
 
-    private String transformWord(String text, boolean includePunctuation, boolean maintainCase) {
+    private String transformWord(String text, boolean includePunctuation, boolean maintainCase, boolean removeDiacritics) {
         if (text == null) {
             return "";
         }
         String transformedText = text;
+
+        if (removeDiacritics) {
+            transformedText = removeDiacritics(transformedText);
+        }
 
         if (!includePunctuation) {
             transformedText = PUNCTUATION_PATTERN.matcher(transformedText).replaceAll("");
@@ -53,5 +59,10 @@ public class TypingService {
             transformedText = transformedText.toLowerCase();
         }
         return transformedText;
+    }
+
+    private String removeDiacritics(String text) {
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return DIACRITIC_PATTERN.matcher(normalized).replaceAll("");
     }
 }
