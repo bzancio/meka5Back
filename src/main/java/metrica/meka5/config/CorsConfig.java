0976fddab352b8d9775f0320e79ad9a16a0d0b76
bzancio <bzancio@gmail.com>
2026-06-07 21:17:30 +0200

    refactor cors

--- src/main/java/metrica/meka5/config/CorsConfig.java
+++ src/main/java/metrica/meka5/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package metrica.meka5;
+package metrica.meka5.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://meka5.bzancio.com") 
+                        .allowedOrigins("https://meka5.bzancio.com", "https://api-meka5.bzancio.com", "http://localhost:4200", "http://localhost:(8080)")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
